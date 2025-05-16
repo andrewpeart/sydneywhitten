dac02d6963b4b0ecebd05e9e691e4d81177146c3
Andrew Peart <andrew@blastingpath.com>
2025-05-16 12:09:21 -0400

    content and color updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ export default {
         muted: 'var(--aw-color-text-muted)',
       },
       fontFamily: {
+        HVSimplicite: ['HVSimplicite', ...defaultTheme.fontFamily.sans],
         sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
         serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
         heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
